3d07fe30c066c7cacf31580575faf9f2650f87a3
SaboorTunio <saboortunio2021@gmail.com>
2025-07-16 20:32:51 +0500

    Update code for gemini format to use gemini api

---
+++ Basic-Ai-Agent/basic-Agent.py
@@ -0,0 +1,35 @@
+import asyncio
+import os
+from dotenv import load_dotenv
+from openai import AsyncOpenAI
+from agents import Agent, OpenAIChatCompletionsModel, Runner, set_tracing_disabled
+
+load_dotenv()
+gemini_api_key = os.getenv("gemini_api_key")
+
+#Reference: https://ai.google.dev/gemini-api/docs/openai
+client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+set_tracing_disabled(disabled=True)
+
+async def main():
+    # This agent will use the custom LLM provider
+    agent = Agent(
+        name="Assistant",
+        instructions="You are a helpful assistant .",
+        model=OpenAIChatCompletionsModel(model="gemini-2.0-flash", openai_client=client),
+    )
+
+    userInput = input("inter your prompt: ")
+    result = await Runner.run(
+        agent,
+        userInput,
+    )
+    print(result.final_output)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
